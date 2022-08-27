eb556023c1294091724e6f59f6d5bd9eecf7c501
abudullah <55946987+abudullah@users.noreply.github.com>
2022-08-28 00:15:46 +0600

    Add files via upload

---
+++ Server/FileHandleling/filepathcraft.js
@@ -0,0 +1,9 @@
+Assetspath={
+    product:"../Assets/Product",
+    Guserprofile:"../Assests/UserImage/Guserprofile",
+    Suppilerprofile:"../Assests/UserImage/Guserprofile"
+}
+
+module.exports={
+    Assetspath
+}
